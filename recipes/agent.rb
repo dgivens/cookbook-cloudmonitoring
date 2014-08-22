1adb1add7cb845878fa72f498e99e407018c6dab
Daniel Givens <daniel.givens@rackspace.com>
2014-08-22 09:37:06 -0500

    Fixed monitoring_id

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -139,7 +139,7 @@ unless node['cloud_monitoring']['agent']['token'].nil?
     group "root"
     mode 0600
     variables(
-      :monitoring_id => node['cloud_monitoring']['agent']['id'],
+      :monitoring_id => node.fqdn,
       :monitoring_token => node['cloud_monitoring']['agent']['token']
     )
   end
